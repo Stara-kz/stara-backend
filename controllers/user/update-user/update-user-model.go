fd732720e1ad44d2debfcaeab7c911e0275e192c
KadirbekSharau <sharaukadr2001@gmail.com>
2022-06-15 13:37:06 +0500

    book apis

--- controllers/user/update-user/update-user-model.go
+++ controllers/user/update-user/update-user-model.go
@@ -2,10 +2,10 @@ package updateUserController
 
 type InputUpdateUserInfo struct {
 	UserID uint `json:"user_id" validate:"required,uuid"`
-	CityID uint `json:"city_id" validate:"required,uuid"`
-	Name string
-	Surname string
-	Phone string
-	Address string
-	AvatarUrl string
+	CityID uint `json:"city_id"`
+	Name string `json:"name"`
+	Surname string `json:"surname"`
+	Phone string `json:"phone"`
+	Address string `json:"address"`
+	AvatarUrl string `json:"avatar_url"`
 }
